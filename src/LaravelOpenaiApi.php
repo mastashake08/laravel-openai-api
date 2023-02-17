b1bc42d7b32a306e1ece8e1e3213c889a899980a
Hacker God <jyrone.parker@gmail.com>
2023-02-17 02:53:51 -0500

    edit main Object

--- src/LaravelOpenaiApi.php
+++ src/LaravelOpenaiApi.php
@@ -2,6 +2,18 @@
 
 namespace Mastashake\LaravelOpenaiApi;
 
+use OpenAI\Laravel\Facades\OpenAI;
+use Mastashake\LaravelOpenaiApi\Models\Prompt;
+
 class LaravelOpenaiApi
 {
+  function generateResult($data) {
+    $data = json_encode($data);
+    $result = OpenAI::completions()->create($data);
+    $prompt = new Prompt([
+      'prompt_text' => $data['prompt_text'],
+      'data' => $data['data']
+    ]);
+    return $prompt;
+  }
 }
