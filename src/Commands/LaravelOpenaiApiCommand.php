f7027cbf2901837956b5893629a5b5f8b86a7535
Hacker God <jyrone.parker@gmail.com>
2023-02-18 00:15:49 -0500

    v1.4.5

--- src/Commands/LaravelOpenaiApiCommand.php
+++ src/Commands/LaravelOpenaiApiCommand.php
@@ -12,6 +12,7 @@ class LaravelOpenaiApiCommand extends Command
 
     public function handle(): int
     {
+        $data = [];
         $suffix = null;
         $n = 1;
         $temperature = 1;
@@ -23,34 +24,39 @@ class LaravelOpenaiApiCommand extends Command
             0
         );
         $prompt = $this->ask('Enter the prompt');
-        if ($this->confirm('Do you wish to add a suffix to the generated result?')) {
-            //
-            $suffix = $this->ask('What is the suffix?');
-        }
+        $data['prompt'] = $prompt;
+
         $model = $this->choice(
             'What model do you want to use?',
             ['text-davinci-003', 'text-curie-001', 'text-babbage-001', 'text-ada-001'],
             0
         );
-        if ($this->confirm('Do you wish to set the max tokens used(defaults to 16)?')) {
-          $max_tokens = (int)$this->ask('Max number of tokens to use?');
+        $data['model'] = $model;
+
+        if($type == 'text') {
+          if ($this->confirm('Do you wish to add a suffix to the generated result?')) {
+              //
+              $suffix = $this->ask('What is the suffix?');
+          }
+          $data['suffix'] = $suffix;
+
+          if ($this->confirm('Do you wish to set the max tokens used(defaults to 16)?')) {
+            $max_tokens = (int)$this->ask('Max number of tokens to use?');
+          }
+          $data['max_tokens'] = $max_tokens;
+
+          if ($this->confirm('Change temperature')) {
+            $temperature = (float)$this->ask('What is the temperature(0-2)?');
+            $data['temperature'] = $temperature;
+          }
         }
+
         if ($this->confirm('Multiple results?')) {
           $n = (int)$this->ask('Number of results?');
+          $data['n'] = $n;
         }
-        if ($this->confirm('Change temperature')) {
-          $temperature = (float)$this->ask('What is the temperature(0-2)?');
-        }
-        $displayJson = $this->confirm('Display JSON results?');
 
-        $data = [
-          'suffix' => $suffix,
-          'prompt' => $prompt,
-          'model' => $model,
-          'max_tokens' => $max_tokens,
-          'n' => $n,
-          'temperature' => $temperature
-        ];
+        $displayJson = $this->confirm('Display JSON results?');
 
         $ai = new LaravelOpenaiApi();
         $result = $ai->generateResult($type,$data);
